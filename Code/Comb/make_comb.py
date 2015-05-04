f60524f291681e722b24b8dcc5e0631ffb959db5
Trevor Walker <trebawa@mit.edu>
2015-05-04 01:55:54 -0400

    Updated comb code; generated sTL for all parts to be printed.

--- Code/Comb/make_comb.py
+++ Code/Comb/make_comb.py
@@ -9,10 +9,10 @@ base_space = base_drawing.modelspace()
 
 #global variables
 #all units in inches
-kerf=0.0 #this should be half the thickness if we're using ponoko
+kerf=0.01 #this should be half the thickness if we're using ponoko
 width = 3.0
 height = 1.25
-tine_width = 3.0/32;
+tine_width = 3.0/64+2*kerf
 #spacing = 
 
 
@@ -44,14 +44,15 @@ def tine(pitch, x, tine_width):
 def make_comb(song_notes,name):#song_notes are midi note numbers (C4=60)
     n = len(song_notes);
     notes = sorted(song_notes)
-    spacing = (width-n*tine_width)/(n-1)
+    spacing = (width-n*tine_width)/n
 
-    y = tine(notes[0],0,tine_width)
+    x = spacing/2
+    y = tine(notes[0],x,tine_width)
     min_y = y
-    polyline(modelspace,[(0,y),(0,0),(width,0)])
-    polyline(base_space,[(tine_width,y),(0,y),(0,0),(width,0)])
-    x=tine_width
-    
+    polyline(modelspace,[(spacing/2,y),(0,y),(0,0),(width,0)])
+    polyline(base_space,[(tine_width+spacing,y),(0,y),(0,0),(width,0)])
+    x += tine_width
+
     for note in notes[1:]:
         #print "processing "+str(note)
         old_y = y
@@ -59,10 +60,10 @@ def make_comb(song_notes,name):#song_notes are midi note numbers (C4=60)
         x += spacing
         y = tine(note,x,tine_width)
         line(modelspace,(old_x,old_y),(x,y))
-        polyline(base_space,[(old_x,old_y),(x,y),(x+tine_width,y)])
         x += tine_width
+        polyline(base_space,[(old_x+spacing/2,old_y),(old_x+spacing/2,y),(x+spacing/2,y)])
 
-    line(modelspace,(width,0),(width,y))
+    polyline(modelspace,[(width,0),(width,y),(width-spacing/2,y)])
     line(base_space,(width,0),(width,y))
 
     modelspace.add_circle((width/4,.25),.0669291,{'color': 0})
@@ -73,4 +74,4 @@ def make_comb(song_notes,name):#song_notes are midi note numbers (C4=60)
     drawing.saveas(str(name)+'_comb.dxf')
     base_drawing.saveas(str(name)+'_base.dxf')
 
-make_comb(range(45,70),"dynspace")
+make_comb([59,60,61,62,64,65,66,67,68,69,70,71,72,74,75,76,77,78,79,80,81,82,83,84,85,86],"harry_potter")
